481e0475b2473c934252c00f21457a9bb5f5e0d7
Joao Paulo S Leite <joaopaulofismed@gmail.com>
2023-02-21 14:29:22 -0300

    database query exception handling

--- backend/src/Controllers/tasksController.js
+++ backend/src/Controllers/tasksController.js
@@ -1,24 +1,48 @@
 const tasksModel = require('../models/tasksModel');
 
 const getAll = async (request, response) => {
-    const tasks = await tasksModel.getAll();
-    return response.status(200).json(tasks);
+    try {
+        const tasks = await tasksModel.getAll();
+        return response.status(200).json(tasks);
+    } catch (error) {
+        return response
+            .status(500)
+            .json({ message: 'Erro ao conectar com o servidor' });
+    }
 };
 const createTask = async (request, response) => {
-    const createdTask = await tasksModel.createTask(request.body);
-    return response.status(201).json(createdTask);
+    try {
+        const createdTask = await tasksModel.createTask(request.body);
+        return response.status(201).json(createdTask);
+    } catch (error) {
+        return response
+            .status(500)
+            .json({ message: 'Erro ao conectar com o servidor' });
+    }
 };
 
 const deleteTask = async (request, response) => {
-    const { id } = request.params;
-    await tasksModel.deleteTask(id);
-    return response.status(204).json();
+    try {
+        const { id } = request.params;
+        await tasksModel.deleteTask(id);
+        return response.status(204).json();
+    } catch (error) {
+        return response
+            .status(500)
+            .json({ message: 'Erro ao conectar com o servidor' });
+    }
 };
 
 const updateTask = async (request, response) => {
-    const { id } = request.params;
-    await tasksModel.updateTask(id, request.body);
-    return response.status(204).json();
+    try {
+        const { id } = request.params;
+        await tasksModel.updateTask(id, request.body);
+        return response.status(204).json();
+    } catch (error) {
+        return response
+            .status(500)
+            .json({ message: 'Erro ao conectar com o servidor' });
+    }
 };
 
 module.exports = {
