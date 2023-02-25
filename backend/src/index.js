25186092a934a4e864fc14c6afce55e9067388da
Joao Paulo S Leite <joaopaulofismed@gmail.com>
2023-02-24 22:22:17 -0300

    test

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ const errors = require('./middlewares/errorsMiddleware');
 const tasksMiddleware = require('./middlewares/tasksMiddleware');
 const express = require('express');
 const router = require('./router');
+require('dotenv').config();
 
 const app = express();
 
@@ -10,4 +11,5 @@ app.use(express.json()); //send Body
 app.use(router);
 app.use(tasksMiddleware.handleUrlNotFound);
 app.use(errors);
-module.exports = app;
+// module.exports = app;
+app.listen(process.env.PORT || 3000);
